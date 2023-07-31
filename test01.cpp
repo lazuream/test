7419c8296e8f69270a7e4eabcb6f725f973abc74
LazureaM <120380841+LazureaM@users.noreply.github.com>
2023-07-31 18:03:44 +0800

    test01

---
+++ test01.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"10"<<endl;
+	
+	return 0;
+}
+
+
+/*
+int main()   //主函数 
+{
+	cout<<"This";
+	cout<<endl<<" is my"<<endl;
+	cout<<" 1st programme";
+	
+	return 0;
+}
+*/
